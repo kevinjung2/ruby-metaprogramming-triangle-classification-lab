64e30d1d6d1a71ca4efe7fb72465eb442ab32c1a
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-28 03:12:34 -0500

    commit tree e0210f0d8e3f07663186d76a43899bb3e5437344

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  attr_accessor :l1, :l2, :l3, :type
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  def kind
+    if self.l1 <= 0 || self.l2 <= 0 || self.l3 <= 0
+      raise TriangleError
+    elsif self.l1 + self.l2 <= self.l3 || self.l2 + self.l3 <= self.l1 || self.l3 + self.l1 <= self.l2
+      raise TriangleError
+    elsif self.l1 == self.l2 && self.l2 == self.l3
+      self.type = :equilateral
+    elsif self.l1 != self.l2 && self.l2 != self.l3 && self.l3 != self.l1
+      self.type = :scalene
+    else
+      self.type = :isosceles
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "oof"
+    end
+  end
 end
